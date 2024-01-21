97c4e8fc210d067b16f8e2304360ed59346b6327
KristanLloyd <kristanlloydbalanga@gmail.com>
2024-01-21 17:05:39 +0800

    added parameters for user and paswword registration

---
+++ flashcardprj/src/Components/Login and Signup/signup.js
@@ -0,0 +1,14 @@
+import React, {useRef,useState,useEffect}from "react";
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+
+const signup = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+export default signup
