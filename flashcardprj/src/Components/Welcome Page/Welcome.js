f6b59395bd082b4a2a500ae8454e5a164ee107c5
Shekinah Marie <131854073+ssmdvc@users.noreply.github.com>
2024-01-22 22:33:45 +0800

    created landing page
    created welcome js and css, home and navbar

---
+++ flashcardprj/src/Components/Welcome Page/Welcome.js
@@ -0,0 +1,12 @@
+import "./Welcome.css";
+import Home from "./Home";
+
+function Welcome() {
+  return (
+    <div className="Welcome">
+      <Home />
+    </div>
+  );
+}
+
+export default Welcome;
