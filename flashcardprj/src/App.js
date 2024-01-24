190f1c408824c5debadaaa8f8ad39e82e3a54f0b
Shekinah Marie <131854073+ssmdvc@users.noreply.github.com>
2024-01-24 14:48:39 +0800

    created landingpage.css
    to store all the css

--- flashcardprj/src/App.js
+++ flashcardprj/src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./Components/Welcome/landingpage.css";
 import Home from "./Components/Welcome/Home";
 import About from "./Components/Welcome/About";
 import Contact from "./Components/Welcome/Contact";
