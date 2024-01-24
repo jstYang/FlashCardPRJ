acfaf90e7c2a7c12960f199e8cc2ed3e13eb3bc2
Shekinah Marie <131854073+ssmdvc@users.noreply.github.com>
2024-01-24 13:38:23 +0800

    added contact js

--- flashcardprj/src/App.js
+++ flashcardprj/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Home from "./Components/Welcome/Home";
 import About from "./Components/Welcome/About";
+import Contact from "./Components/Welcome/Contact";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Contact />
     </div>
   );
 }
