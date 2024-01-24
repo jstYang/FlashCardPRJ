2b9d699c4a209f8f91bf49c7175d9d5f9cad740f
Shekinah Marie <131854073+ssmdvc@users.noreply.github.com>
2024-01-24 21:49:32 +0800

    added landingpage

--- flashcardprj/src/App.js
+++ flashcardprj/src/App.js
@@ -1,16 +1,10 @@
 import "./Components/Welcome/landingpage.css";
-import Home from "./Components/Welcome/Home";
-import About from "./Components/Welcome/About";
-import Contact from "./Components/Welcome/Contact";
-import Footer from "./Components/Welcome/Footer";
+import "./Components/Welcome/landingpage";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <About />
-      <Contact />
-      <Footer />
+      <landingpage />
     </div>
   );
 }
---
+++ flashcardprj/src/Components/Welcome/landingpage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const landingpage = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
