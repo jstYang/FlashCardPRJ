26937cbf35d646c470caf60757e3ecced67b311b
Shekinah Marie <131854073+ssmdvc@users.noreply.github.com>
2024-01-24 13:22:30 +0800

    update app.css,js
    added about js

--- flashcardprj/src/App.js
+++ flashcardprj/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./Components/Welcome/Home";
+import About from "./Components/Welcome/About";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <About />
     </div>
   );
 }
