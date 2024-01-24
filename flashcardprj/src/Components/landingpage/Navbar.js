1cccb4099998284ab89734b34ae7028d18cfad37
Shekinah Marie <131854073+ssmdvc@users.noreply.github.com>
2024-01-24 18:04:14 +0800

    added assets and updated landingpage

---
+++ flashcardprj/src/Components/landingpage/Navbar.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuOptions = [
+    {
+      text: "Home",
+    },
+    {
+      text: "About",
+    },
+    {
+      text: "Contact",
+    },
+  ];
+  return (
+    <nav>
+      <div className="nav-logo-container"></div>
+      <div className="navbar-links-container">
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+        <a href=""></a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- flashcardprj/src/Components/landingpage/landingpage.js
+++ flashcardprj/src/Components/landingpage/landingpage.js
@@ -1,7 +1,19 @@
 import React, { useState } from "react";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer";
 
-const Landingpage = () => {
-  return <div>landingpage</div>;
-};
+function Landingpage() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
 
 export default Landingpage;
