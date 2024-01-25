db22bddba74bd9826c195f951cc65a544640ff5d
jstYang <130741846+jstYang@users.noreply.github.com>
2024-01-25 09:49:05 +0800

    deleted unnecesarry code

--- flashcardprj/src/Components/landingpage/landingpage.js
+++ flashcardprj/src/Components/landingpage/landingpage.js
@@ -2,10 +2,6 @@ import React from "react";
 import BannerBackground from "../Assets/home-banner-background.png";
 import AboutBackground from "../Assets/about-background.png";
 import { Link } from "react-router-dom";
-import { BsTwitter } from "react-icons/bs";
-import { SiLinkedin } from "react-icons/si";
-import { BsYoutube } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
 import { CgAdd } from "react-icons/cg";
 import "./landingpage.css";
 
@@ -60,46 +56,16 @@ function Landingpage() {
         </div>
       </div>
     
-    </div>
-      
-    <div className="footer-wrapper">
-        
-        <div className="footer-section-one">
-          <div className="footer-logo-container"></div>
-          <div className="footer-icons">
-            <BsTwitter />
-            <SiLinkedin />
-            <BsYoutube />
-            <FaFacebookF />
-          </div>
         </div>
-
-        <div className="footer-section-two">
-
-          <div className="footer-section-columns">
-            <span>Home</span>
-            <span>Help</span>
-            <span>Share</span>
-            <span>Carrers</span>
-            <span>Testimonials</span>
-            <span>Work</span>
-          </div>
-
-          <div className="footer-section-columns">
-            <span>Dorothea Avellana</span>
-            <span>Kristan Lloyd Balanga</span>
-            <span>Shekinah Marie Cayco</span>
-          </div>
-
-          <div className="footer-section-columns">
-            <span>Terms & Conditions</span>
-            <span>Privacy Policy</span>
-          </div>
-          
+      
+    
+        
+        <div className="footer-section">
+          <p>Copyright &#169; 2024 FlashFocus. All Rights Reserved.</p>
         </div>
       </div>
 
-    </div>
+   
 
   );
 }
